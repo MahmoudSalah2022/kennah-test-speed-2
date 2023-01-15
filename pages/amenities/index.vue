f478722718c7f10d5db5609c99d3dcd1c6d7cacd
DESKTOP-JOB5LDJ\pc <mahmoudeahmed.salah@gmail.com>
2023-01-15 02:23:33 +0200

    14/1 edit

--- pages/amenities/index.vue
+++ pages/amenities/index.vue
@@ -3,38 +3,43 @@
   
   <v-row class="grey lighten-4" flat>
       <sidebar/>
-      <v-col id="aparts-section" class="ma-4 pa-0" >
-          <v-col id="welcome-aparts-section" class="ma-0 pa-0">
+      <v-col id="Amenity-section" class="ma-4 pa-0" >
+          <v-col id="welcome-Amenity-section" class="ma-0 pa-0">
               <h3>Welecom In Your Amenities</h3>
           </v-col>
-          <v-col id="add-amenitie" class="my-4 pa-0" >
+          <v-col id="add-Amenity" class="my-4 pa-0" >
                   <v-btn elevation="2" color="fcolor white--text" @click="dialog = true">
-                      Add Amenities
+                      Add Amenity
                   </v-btn>
           </v-col>
-          <v-col id="amenities-list" class="ma-0 pa-0">
+          <v-col id="Amenities-list" class="ma-0 pa-0"  col="12" xl="1" sm="3" lg="1" md="2" xs="1">
                   <v-simple-table>
                       <template v-slot:default>
-                      <thead>
-                          <tr>
-                            <th class="text-left">
-                                Name
-                            </th>
-                          </tr>
-                      </thead>
-                                  <tbody v-for="(amenitie, i) in amenities" :key="i">
-                                      <tr>
-                                      <td>{{ amenitie.Name }}</td>
-                                      </tr>
-                                  </tbody>
-                          
+                        <thead>
+                            <tr >
+                              <th class="text-left headicon ma-0" >
+                                  Icon
+                              </th>
+                              <th class="text-left">
+                                  Name
+                              </th>
+
+                            </tr>
+                        </thead>
+                        <tbody v-for="(Amenity, i) in Amenities" :key="i">
+                                        <tr>
+                                          <td class="headicon ma-0"><v-icon>{{ Amenity.Icon }}</v-icon></td>
+                                          <td>{{ Amenity.Name }}</td>
+                                          
+                                        </tr>
+                        </tbody>     
                       </template>
                   </v-simple-table>
           </v-col>
           <v-dialog v-model="dialog" max-width="400px">
               <v-card>
                 <v-toolbar elevation="0">
-                  <v-toolbar-title>New Amenitie</v-toolbar-title>
+                  <v-toolbar-title>New Amenity</v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-btn icon @click.stop="dialog = false" class="mr-0">
                     <v-icon>mdi-close</v-icon>
@@ -43,7 +48,8 @@
                 <v-divider></v-divider>
                 <v-card-text v-if="dialog" class="pa-4">
                   <v-form ref="form" v-model="form.valid" v-on:submit.prevent="submitForm">
-                    <v-text-field v-model="form.fields.Name" :rules="form.rules.Name" label="Amenitie Name" outlined></v-text-field>
+                    <v-text-field v-model="form.fields.Name" :rules="form.rules.Name" label="Amenity Name" outlined></v-text-field>
+                    <v-text-field v-model="form.fields.Icon" :rules="form.rules.Icon" label="Amenity Icon" outlined></v-text-field>
                     <v-btn depressed color="primary" type="submit" :disabled="!form.valid"><span>Add</span></v-btn>
                   </v-form>
                 </v-card-text>
@@ -55,18 +61,32 @@
   </v-app>
 </template>
 
+<style>
+
+.headicon {
+    max-width: 50px !important;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+}
+
+</style>
+
 <script>
 export default {
   layout:'dashboard',
   data: () => ({
-    amenities: [],
+    Amenities: [],
     dialog: false,
     form: {
       valid: false,
-      fields: { Name: "" },
+      fields: { Name: "" ,Icon:""},
       error:[],
       rules: {
         Name: [(v) => !!v || ' This field is required'],
+        Icon: [(v) => !!v || ' This field is required'],
       },
     },
   }),
@@ -74,10 +94,13 @@ export default {
   methods: {    
     
     async submitForm() {
-      const Name = this.form.fields.Name;
-      await this.$axios.$post("amenities", { Name }).then((res) => {
+     const data={
+         Name : this.form.fields.Name,
+         Icon : this.form.fields.Icon,
+      }
+      await this.$axios.$post("Amenities", data ).then((res) => {
         if (res._id) {
-          this.amenities.push(res);
+          this.Amenities.push(res);
           this.dialog = false;
           this.$refs.form.reset();
         } 
@@ -88,10 +111,10 @@ export default {
 
   },
   async fetch() {
-          await this.$axios.$get("amenities").then((res) => {
+          await this.$axios.$get("Amenities").then((res) => {
           console.log(res);
           if (res?.length > 0) {
-              this.amenities = res;
+              this.Amenities = res;
           }
           });
       },
--- pages/customers-board/index.vue
+++ pages/customers-board/index.vue
@@ -5,13 +5,8 @@
       <sidebar/>
       <v-col id="customer-section" class="ma-4 pa-0">
           <v-col id="welcome-customer-section" class="ma-0 pa-0">
-              <h3>Welecom In Your Customers</h3>
+              <h3>Welcome In Your Customers</h3>
           </v-col>
-          <!-- <v-col id="add-apartment" class="my-4 pa-0" >
-                  <v-btn elevation="2" color="fcolor white--text" to="/add-Customers">
-                      Add Apartment
-                  </v-btn>
-          </v-col> -->
           <v-col id="Customers-list" class="ma-0 pa-0">
                   <v-simple-table >
                       <template v-slot:default>
@@ -27,13 +22,13 @@
                               Last Reservation
                           </th>
                           <th class="text-left">
-                              Statu
+                              Statue
                           </th>
                           </tr>
                       </thead>
                                   <tbody v-for="(customer, i) in customers" :key="i">
                                       <tr>
-                                      <td>{{ customer.FristName }}</td>
+                                      <td>{{ customer.FirstName }}</td>
                                       <td>{{ customer.Email }}</td>
                                       <td>{{ customer.LastReservation }}</td>
                                       <td>{{ customer.Status }}</td>
@@ -84,176 +79,11 @@
           </v-col> 
       </v-col>
   </v-row>
-  <v-dialog v-model="editDialog" max-width="1360px">
-    <v-card>
-      <v-toolbar elevation="0">
-        <v-toolbar-title>Edit Apartment</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon @click.stop="editDialog = false " class="mr-0">
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-divider></v-divider>
-      <v-card-text v-if="editDialog" class="pa-4">
-        <v-form ref="form" v-model="form.valid" v-on:submit.prevent="editForm">
-          <v-col id="add-section" class="ma-0 pa-0 d-flex flex-column" cols="12" sm="8">
-                      <v-row class="d-flex flex-row ma-0" >
-                      <v-col id="ApartName" class="d-flex flex-column mt-5 mr-3 pa-0">
-                          <h4>Apartment Name</h4> 
-                          <v-text-field
-                          v-model="form.fields.Name" :rules="form.rules.Name"
-                          class="mt-3"
-                          label="Apartment Name"
-                          placeholder="pleas insert apartment name"
-                          outlined
-                          ></v-text-field>
-                      </v-col>
-                      <v-col id="CompoundName" class="d-flex flex-column mt-5 pa-0">
-                          <h4>Compound Name</h4> 
-                          <v-text-field
-                          v-model="form.fields.Compound" :rules="form.rules.Compound"
-                          class="mt-3"
-                          label="Compound Name"
-                          placeholder="pleas insert compound name"
-                          outlined
-                          ></v-text-field>
-                      </v-col>
-                      </v-row>
-                      <v-row class="d-flex flex-row ma-0">
-                          <v-col id="RoomNumber" class="d-flex flex-column mt-5 pa-0">
-                              <h4>Number Of Rooms</h4> 
-                              <v-text-field
-                              v-model="form.fields.NumberRooms" :rules="form.rules.NumberRooms"
-                              class="mt-3"
-                              label="Number Of Rooms"
-                              placeholder="pleas insert Number Of Rooms"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                          <v-col id="BathRoomNumber" class="d-flex flex-column mt-5 mx-3 pa-0">
-                              <h4>Number Of BathRooms</h4> 
-                              <v-text-field
-                              v-model="form.fields.NumberBathRooms" :rules="form.rules.NumberBathRooms"
-                              class="mt-3"
-                              label="Number Of BathRooms"
-                              placeholder="pleas insert Number Of BathRooms"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                          <v-col id="GuestNumber" class="d-flex flex-column mt-5  pa-0">
-                              <h4>Number Of Guest</h4> 
-                              <v-text-field
-                              v-model="form.fields.NumberGuest" :rules="form.rules.NumberGuest"
-                              class="mt-3"
-                              label="Number Of Guest"
-                              placeholder="pleas insert Number Of Guest"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                      </v-row>
-                      <v-row class="d-flex flex-row ma-0">
-                          <v-col id="Location" class="d-flex flex-column mt-5 pa-0">
-                              <h4>Apartment Address</h4> 
-                              <v-text-field
-                              v-model="form.fields.Location" :rules="form.rules.Location"
-                              class="mt-3"
-                              label="Apartment Address"
-                              placeholder="pleas insert Apartment Address"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                      </v-row>
-                      <v-row class="d-flex flex-row ma-0">
-                          <v-col id="NightPrice" class="d-flex flex-column mt-5  pa-0">
-                              <h4>Night Price</h4> 
-                              <v-text-field
-                              v-model="form.fields.Price" :rules="form.rules.Price"
-                              class="mt-3"
-                              label="Night Price"
-                              placeholder="pleas insert Night Price"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                          <v-col id="CleanFees" class="d-flex flex-column mt-5 mx-3 pa-0">
-                              <h4>Clean Fees</h4> 
-                              <v-text-field
-                              v-model="form.fields.Clean" :rules="form.rules.Clean"
-                              class="mt-3"
-                              label="Clean Fees"
-                              placeholder="pleas insert Clean Fees"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                          <v-col id="CoverFees" class="d-flex flex-column mt-5 pa-0">
-                              <h4>Cover Fees</h4> 
-                              <v-text-field
-                              v-model="form.fields.Cover" :rules="form.rules.Cover"
-                              class="mt-3"
-                              label="Cover Fees"
-                              placeholder="pleas insert Cover Fees"
-                              outlined
-                              ></v-text-field>
-                          </v-col>
-                      </v-row>
-                      <v-divider></v-divider>
-                      <h3 class="my-4">Amenities</h3>
-                      <v-row class="d-flex flex-column ma-0 mb-3">
-                          <v-col id="Amenities" class="d-flex flex-column mt-5 pa-0">
-                              <h4>Choose Amenities</h4>
-                              <v-col class="d-flex flex-row ma-0 pa-0" col="12">
-                                  <v-col v-for="(amenitie, i) in amenities" :key="i" class="ma-0 pa-0" md="2" width="50" max-width="100">
-                                      <v-checkbox
-                                      v-model="selected"
-                                      :label="`${ amenitie.Name }`"
-                                      >{{ amenitie.Name }}</v-checkbox>
-                                  </v-col>
-                              </v-col>
- 
-                          </v-col>
-                          <v-col id="FavAmenity" class="d-flex flex-column mt-5  pa-0">
-                              <h4>Choose a Fav Amenity</h4> 
-                          </v-col>
-                      </v-row>
-                      <v-divider calss="mt-2"></v-divider>
-                      <h3 class="my-4">Media</h3>
-                      <v-row class="d-flex flex-row ma-0">
-                          <v-col id="MainImage" class="d-flex flex-column mt-3 pa-0">
-                              <h4 class="mb-3">Main Image</h4> 
-                              <v-file-input
-                                  label="pleas insert Main Image"
-                                  filled
-                                  prepend-icon="mdi-wallpaper"
-                              ></v-file-input>
-                          </v-col>
-                          <v-col id="ImagesGallary" class="d-flex flex-column mt-3 mx-3  pa-0">
-                              <h4 class="mb-3">Apartment Images</h4> 
-                              <v-file-input
-                                  label="pleas insert Apartment Images"
-                                  multiple
-                                  filled
-                                  prepend-icon="mdi-view-gallery"
-                              ></v-file-input>
-                          </v-col>
-                          <v-col id="ApartVideo" class="d-flex flex-column mt-3  pa-0">
-                              <h4 class="mb-3">Apartment Video</h4> 
-                              <v-file-input
-                                  label="pleas insert Apartment Video"
-                                  filled
-                                  prepend-icon="mdi-video"
-                              ></v-file-input>
-                          </v-col>
-                      </v-row>
-          </v-col>
-          <v-btn depressed color="primary" type="submit" :disabled="!form.valid"><span>Edit</span></v-btn>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
-  <v-row justify="center">
+  <v-row justify="center" id="sign-up">
       <v-dialog v-model="registerdialog" persistent max-width="600px">
           <template v-slot:activator="{ on, attrs }">
               <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                  Open Dialog
+                  Sign Up
               </v-btn>
           </template>
           <v-card >
@@ -321,10 +151,64 @@
               </v-card-text>
               <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="dialog = false">
+                  <v-btn color="blue darken-1" text @click="registerdialog = false">
                       Close
                   </v-btn>
-                  <v-btn color="blue darken-1" text @click="dialog = false">
+                  <v-btn color="blue darken-1" text @click="registerdialog = false">
+                      Save
+                  </v-btn>
+              </v-card-actions>
+          </v-card>
+      </v-dialog>
+  </v-row>
+  <v-row justify="center" id="sign-in">
+      <v-dialog v-model="signindialog" persistent max-width="600px">
+          <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                  Sign in
+              </v-btn>
+          </template>
+          <v-card >
+              <v-card-title>
+                  <span class="text-h5">Sign In</span>
+              </v-card-title>
+              <v-card-text>
+                  <v-container>
+                      <v-row col="12" md="10" max-width="600px" class="d-block">
+                                <v-row class="d-flex flex-row ma-0" >
+                                    <v-col id="ApartName" class="d-flex flex-column mt-5 mr-3 pa-0">
+                                        <h4>Your Email</h4> 
+                                        <v-text-field
+                                        v-model="form.fields.Name" :rules="form.rules.Name"
+                                        class="mt-3"
+                                        label="Insert Your Email"
+                                        placeholder="pleas Insert Your Email"
+                                        outlined
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <v-row class="d-flex flex-row ma-0" >
+                                    <v-col id="ApartName" class="d-flex flex-column mt-5 mr-3 pa-0">
+                                        <h4>Your Password</h4> 
+                                        <v-text-field
+                                        v-model="form.fields.Name" :rules="form.rules.Name"
+                                        class="mt-3"
+                                        label="Insert Your Password"
+                                        placeholder="pleas Insert Your Password"
+                                        outlined
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                      </v-row>
+                  </v-container>
+                  <small>*indicates required field</small>
+              </v-card-text>
+              <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="signindialog = false">
+                      Close
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="signindialog = false">
                       Save
                   </v-btn>
               </v-card-actions>
@@ -340,7 +224,6 @@
     data: () => ({
     e6: 1,
     customers: [],
-    editDialog: false,
     editCustomerId: null,
     form: {
       valid: false,
@@ -353,6 +236,7 @@
       },
     },
     registerdialog:false,
+    signindialog:false,
   }),
 
     methods: {              
